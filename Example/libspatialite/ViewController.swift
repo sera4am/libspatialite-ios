2e79c381c31e0dde1720a177ac33ada1f4c6e2b6
t.kazama <takeo@kazama.biz>
2020-07-09 13:40:24 +0900

    Initial commit

---
+++ Example/libspatialite/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  libspatialite
+//
+//  Created by sera4am on 07/09/2020.
+//  Copyright (c) 2020 sera4am. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
